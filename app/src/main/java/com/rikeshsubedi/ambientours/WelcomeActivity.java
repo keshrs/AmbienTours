8eb7c3228f242bf932d49abf90a625ddf1acd380
keshrs <keshrs@gmail.com>
2018-04-06 18:03:56 -0400

    Create action for begin button, add interest selection activity.

--- app/src/main/java/com/rikeshsubedi/ambientours/WelcomeActivity.java
+++ app/src/main/java/com/rikeshsubedi/ambientours/WelcomeActivity.java
@@ -1,17 +1,27 @@
 package com.rikeshsubedi.ambientours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
+
+        final Button btnBegin = findViewById(R.id.btnBegin);
+        btnBegin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent gotoOptions = new Intent(WelcomeActivity.this, InterestSelectionActivity.class);
+                startActivity(gotoOptions);
+                finish();
+            }
+        });
     }
 }
