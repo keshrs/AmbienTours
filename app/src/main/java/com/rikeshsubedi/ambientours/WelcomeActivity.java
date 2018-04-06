0f130d226653cfc357d4726ec4a4264d44b10de2
keshrs <keshrs@gmail.com>
2018-04-06 12:24:47 -0400

    Add welcome screen.

--- app/src/main/java/com/rikeshsubedi/ambientours/WelcomeActivity.java
+++ app/src/main/java/com/rikeshsubedi/ambientours/WelcomeActivity.java
@@ -2,12 +2,16 @@ package com.rikeshsubedi.ambientours;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
     }
 }
