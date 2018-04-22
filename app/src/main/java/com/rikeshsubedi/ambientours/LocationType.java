75febe63b9054b72865bf35a3fba17a5c42059c5
keshrs <keshrs@gmail.com>
2018-04-22 09:26:10 -0400

    Update location enum.

--- app/src/main/java/com/rikeshsubedi/ambientours/LocationType.java
+++ app/src/main/java/com/rikeshsubedi/ambientours/LocationType.java
@@ -1,5 +1,20 @@
 package com.rikeshsubedi.ambientours;
 
+/**
+ * Enum representing each type of location a user can choose to hear about.
+ * @author Rikesh Subedi
+ */
 public enum LocationType {
-    ART, ENTERTAINMENT, HISTORY, NATURE, COMMERCE, TOURIST
+    ART ("Art and Culture"),
+    HISTORY ("History"),
+    COMMERCE ("Restaurants and Commerce"),
+    NATURE ("Nature"),
+    ENTERTAINMENT ("Entertainment"),
+    TOURIST ("Top Tourist Attractions");
+
+    private final String title;
+
+    LocationType(String title) {
+        this.title = title;
+    }
 }
