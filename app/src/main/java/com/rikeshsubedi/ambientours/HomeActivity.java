0d776f87b7aaee60dc4e4289343f8018a774d874
keshrs <keshrs@gmail.com>
2018-04-22 00:26:09 -0400

    Create interaction for selection screen.

--- app/src/main/java/com/rikeshsubedi/ambientours/HomeActivity.java
+++ app/src/main/java/com/rikeshsubedi/ambientours/HomeActivity.java
@@ -1,16 +1,39 @@
 package com.rikeshsubedi.ambientours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    /* These variables contain the state of the tour parameters */
+    boolean isArt = false;
+    boolean isEntertainment = false;
+    boolean isHistory = false;
+    boolean isNature = false;
+    boolean isCommerce = false;
+    boolean isTourist = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Intent intent = getIntent();
+        Bundle intentBundle = intent.getExtras();
+        if (intentBundle != null) {
+            isArt = intentBundle.getBoolean("art");
+            isEntertainment = intentBundle.getBoolean("entertainment");
+            isHistory = intentBundle.getBoolean("history");
+            isNature = intentBundle.getBoolean("nature");
+            isCommerce = intentBundle.getBoolean("commerce");
+            isTourist = intentBundle.getBoolean("tourist");
+        }
 
-
+        String text = isArt + " " + isEntertainment + " " + isHistory + " " + isNature + " "
+                + isCommerce + " " + isTourist;
+        TextView tvTest = findViewById(R.id.tvTest);
+        tvTest.setText(text);
     }
 }
--- app/src/main/java/com/rikeshsubedi/ambientours/InterestSelectionActivity.java
+++ app/src/main/java/com/rikeshsubedi/ambientours/InterestSelectionActivity.java
@@ -1,8 +1,13 @@
 package com.rikeshsubedi.ambientours;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class InterestSelectionActivity extends AppCompatActivity {
@@ -23,6 +28,45 @@ public class InterestSelectionActivity extends AppCompatActivity {
         for (int image: images) {
             flipperImages(image);
         }
+
+        final CheckBox chkArt = findViewById(R.id.chkArtCulture);
+        final CheckBox chkEntertainment = findViewById(R.id.chkEntertainment);
+        final CheckBox chkHistory = findViewById(R.id.chkHistory);
+        final CheckBox chkNature = findViewById(R.id.chkNature);
+        final CheckBox chkCommerce = findViewById(R.id.chkRestaurantsCommerce);
+        final CheckBox chkTourist = findViewById(R.id.chkTouristAttractions);
+        final Button btnTour = findViewById(R.id.btnTour);
+        btnTour.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                boolean wantArt = chkArt.isChecked();
+                boolean wantEntertainment = chkEntertainment.isChecked();
+                boolean wantHistory = chkHistory.isChecked();
+                boolean wantNature = chkNature.isChecked();
+                boolean wantCommerce = chkCommerce.isChecked();
+                boolean wantTourist = chkTourist.isChecked();
+
+                if (!wantArt && !wantEntertainment && !wantHistory && !wantNature && !wantCommerce
+                        && !wantTourist) {
+                    // Handle if no items selected
+                    String warning = "Select at least one topic.";
+                    Toast.makeText(InterestSelectionActivity.this, warning, Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent goHome = new Intent(InterestSelectionActivity.this,
+                            HomeActivity.class);
+
+                    Bundle bundle = new Bundle();
+                    bundle.putBoolean("art", wantArt);
+                    bundle.putBoolean("entertainment", wantEntertainment);
+                    bundle.putBoolean("history", wantHistory);
+                    bundle.putBoolean("nature", wantNature);
+                    bundle.putBoolean("commerce", wantCommerce);
+                    bundle.putBoolean("tourist", wantTourist);
+                    goHome.putExtras(bundle);
+
+                    startActivity(goHome);
+                }
+            }
+        });
     }
 
     /**
